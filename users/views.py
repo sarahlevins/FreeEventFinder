29a3baad8e63db727849470255dd5e472966bf27
sarahlevins <sarahjeanlevins@gmail.com>
2019-09-23 21:36:46 +0800

    added list of hosted events to user prof pg

--- users/views.py
+++ users/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic.detail import SingleObjectMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from .forms import CustomUserCreationForm
 from .models import CustomUser
+from eventFinderApp.models import Event
 
-class Register(generic.CreateView):
+class Register(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/register.html'
@@ -16,6 +18,6 @@ class Login(LoginView):
 class Logout(LogoutView):
     pass
 
-class UserDetailView(generic.DetailView):
+class UserDetailView(DetailView):
     model = CustomUser
     template_name = 'account.html'
